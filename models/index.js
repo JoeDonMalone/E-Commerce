eda82167c78de39563b1220cfca0eb07dff425f6
Joe Malone <joe.d.malone@gmail.com>
2021-05-24 00:05:46 -0500

    env File configured, server js File configured, models partially configured

---
+++ models/index.js
@@ -0,0 +1,42 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+})
+
+// Categories have many Products
+Category.hasMany(Products, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+})
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model:ProductTag,
+    unique:false
+  }, 
+
+  as: 'Product_tags'
+})
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
